3a23095171e251295b0ff9cda7e63024fbc36564
jiale1379 <56367511+jiale1379@users.noreply.github.com>
2021-04-10 15:42:11 -0500

    Update EmpHome.vue

--- project-frontend/src/components/EmpHome.vue
+++ project-frontend/src/components/EmpHome.vue
@@ -5,7 +5,7 @@
     <el-header>
       <div>
         <img src="../assets/logo.png" alt />
-        <span>Shapes Brow Bar Management System (Employee)</span>
+        <span>Shapes Brow Bar Management System (Staff)</span>
       </div>
       <el-button type="info" @click="logout">Logout</el-button>
     </el-header>
